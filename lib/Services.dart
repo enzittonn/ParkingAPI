b22874d62407c3af8f4a0edfd0c916aa1b30e33d
Enzo <enzito@protonmail.com>
2020-05-08 15:44:20 +0200

    o....k

--- lib/Services.dart
+++ lib/Services.dart
@@ -1,20 +1,23 @@
-/*
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
 import 'ParkingSpace.dart';
 
 class Services {
-  static const String url = 'https://openparking.stockholm.se/LTF-Tolken/v1/pbuss/within?radius=100&lat=59.32784&lng=18.05306&outputFormat=json&apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97';
 
-  static Future<List> getParkingSpace() async {
-    try {
-      final response = await http.get(url);
-      if (response.statusCode == 200) {
-        final List parkingspaces = parkingSpaceFromJson(response.body);
-        return parkingspaces;
-      }
-    } catch (e) {
-      return List<Parkering>();
+
+  static Future<Parkering> fetchParkering() async {
+    final response = await http.get(
+        'https://openparking.stockholm.se/LTF-Tolken/v1/pbuss/within?radius=100&lat=59.32784&lng=18.05306&outputFormat=json&apiKey=c9e27b4b-e374-41b5-b741-00b90cbe2d97');
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return Parkering.fromJson(json.decode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load parkering');
     }
-
   }
-}*/
+}
